5f470a25ae9cfe8021ad4866900be80080beffab
Cody Davis <davis.r.cody@gmail.com>
2022-06-16 18:45:26 -0700

    added ssl config

--- models/index.js
+++ models/index.js
@@ -7,7 +7,14 @@ const interfaceModel = require("./Interface");
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL || `postgres://${process.env.USERNAME}:${process.env.PW}${process.env.HOST}:5432/${process.env.DB_NAME}`
 
-const sequelize = new Sequelize(DATABASE_URL)
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
 
 // use this code when deploying
 // {
