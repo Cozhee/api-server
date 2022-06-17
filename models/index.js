eff6d9e76cc3b4bd4b8a3254e2e5bb1524908a0b
Cody Davis <davis.r.cody@gmail.com>
2022-06-16 19:11:18 -0700

    who knows

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ const interfaceModel = require("./Interface");
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL || `postgres://${process.env.USERNAME}:${process.env.PW}${process.env.HOST}:5432/${process.env.DB_NAME}`
 
+// const sequelize = new Sequelize(DATABASE_URL)
+
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
     ssl: {
@@ -15,16 +17,6 @@ const sequelize = new Sequelize(DATABASE_URL, {
     }
   }
 })
-
-// use this code when deploying
-// {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//         rejectUnauthorized: false
-//     }
-//   }
-// }
 
 const FoodModel = FoodSchema(sequelize, DataTypes)
 const ClothesModel = ClothesSchema(sequelize, DataTypes)
